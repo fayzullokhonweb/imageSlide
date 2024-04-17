8a0d04dbb66f178bba9fb933ef0f986385df08d1
rasulov fayzullaxon <rasulov@gmail.com>
2024-04-18 03:45:59 +0500

    imageSlide

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const ImageSlider = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToNextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const goToPreviousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent:"center", alignItems:"center", flexDirection: "column", gap: 8 }}>
+      <img
+        src={images[currentImageIndex]}
+        alt={`Images`}
+        style={{ display: "block", width: 400, marginBottom: 20, height: 450 }}
+      />
+      <div style={{ display: "flex", gap: 12 }}>
+        <button onClick={goToPreviousImage}>Back</button>
+        <button onClick={goToNextImage}>Next</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default ImageSlider;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import ImageSlider from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const images = [
+  "./assets/img.jpg",
+  "./assets/img2.jpg",
+  "./assets/img3.jpg",
+  "./assets/img4.jpg",
+];
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ImageSlider images={images} />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
